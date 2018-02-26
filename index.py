85812726552449d8fdeb2e84fbbdf6737da5d8cd
Naveen Piedy <naveenpiedy@users.noreply.github.com>
2018-02-26 15:49:25 -0700

    Isolated scorecard.

--- index.py
+++ index.py
@@ -9,5 +9,18 @@ soup = bs.BeautifulSoup(sauce, 'lxml')
 
 status = soup.find('div', class_='cb-scrcrd-status')
 innings_1 = soup.find(id='innings_2')
-print(innings_1.text)
-print(status.text)
+text_gray = innings_1.find_all('span', class_= 'text-gray')
+sbitems = innings_1.find_all('a', class_='cb-text-link')
+items = innings_1.find_all
+# for i, j in zip(sbitems, text_gray):
+#     print(i.text+"      "+j.text)
+#     #print(j.text)
+
+cbscrditems = innings_1.find_all('div', class_='cb-scrd-itms')
+
+for i in cbscrditems:
+    print(i.text)
+# for i in text_gray:
+#     print(i.text)
+# print(innings_1.text)
+# print(status.text)
