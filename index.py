45f4d38746e767e892d8c846c99aba2dc374c160
Naveen Piedy <naveenpiedy@users.noreply.github.com>
2018-02-27 10:56:40 -0700

    Added Graph

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import bs4 as bs
 import urllib.request
+import matplotlib.pyplot as plt
 import regex as re
 
 
@@ -18,9 +19,29 @@ items = innings_1.find_all
 
 cbscrditems = innings_1.find_all('div', class_='cb-scrd-itms')
 
+labels = []
+sizes =[]
+outs=[]
+
 for i in cbscrditems:
-    print(i.text)
+    sbitems = i.find('a', class_='cb-text-link')
+    if sbitems:
+        labels.append(sbitems.text)
+    text_gray = i.find('span', class_ = 'text-gray')
+    if text_gray:
+        outs.append(text_gray.text)
+    bold = i.find('div', class_='text-bold')
+    if bold:
+        sizes.append(int(bold.text))
 # for i in text_gray:
 #     print(i.text)
 # print(innings_1.text)
 # print(status.text)
+    #print(i.text)
+print(len(labels), len(sizes))
+fig1, ax1 = plt.subplots()
+
+ax1.pie(sizes[:len(outs)],explode=None, labels=None, autopct='%0.1f%%', shadow=True, startangle=90)
+ax1.legend(labels= labels[:len(outs)])
+ax1.axis('equal')
+plt.show()
